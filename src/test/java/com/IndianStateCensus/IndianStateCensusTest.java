e5f71151e0996bd5a45f3761cfd53f00be8d8645
leena0203 <leenasarode02@gmail.com>
2020-10-25 23:17:02 +0530

    Refractor: Test for wrong header throws custom exception

--- src/test/java/com/IndianStateCensus/IndianStateCensusTest.java
+++ src/test/java/com/IndianStateCensus/IndianStateCensusTest.java
@@ -11,6 +11,7 @@ class IndianStateCensusTest {
 	String	Incorrect_CSVFile = "C:\\Users\\leena\\eclipse-workspace\\Indian StateCensus\\IndiaStateCensusData.csv";
 	String	Incorrect_CSVFile_Path = "C:\\Users\\leena\\eclipse-workspace\\IndiaStateCensusData.txt";
 	String  Incorrect_Delimiter = "C:\\Users\\leena\\eclipse-workspace\\indiastatecensusdata.csv";
+	String Incorrect_header = "C:\\Users\\leena\\eclipse-workspace\\IndianStateCensus\\Indian_State_Censun.csv";
 
 	/**
 	 * UC1_TC 1.1_Returns number of records in datacsv file
@@ -68,6 +69,22 @@ class IndianStateCensusTest {
 		int count = 0;
 		try {
 			count = test.loadStateCSVData(Incorrect_Delimiter);
+		} catch (CensusAnalyserExecption e) {
+			//e.printStackTrace();
+			assertEquals(CensusAnalyserExecption.ExceptionType.INCORRECT_FILE, e.type);
+		}
+	}
+	
+	/**
+	 * UC1_TC 1.5_when Wrong header csv file is passed throws custom exception
+	 * @throws IOException
+	 */
+	@Test
+	void givenWrongHeaderFile_ReturnsCustomException() throws IOException {
+		StateCensusAnalyser test = new StateCensusAnalyser();
+		int count = 0;
+		try {
+			count = test.loadStateCSVData(Incorrect_header);
 		} catch (CensusAnalyserExecption e) {
 			//e.printStackTrace();
 			assertEquals(CensusAnalyserExecption.ExceptionType.INCORRECT_FILE, e.type);
