281c98e2c7d0bb50a23918f62fb4d7314382e6a4
leena0203 <leenasarode02@gmail.com>
2020-10-25 23:00:39 +0530

    Refractor: Test for wrong extension throws custom exception

--- src/test/java/com/IndianStateCensus/IndianStateCensusTest.java
+++ src/test/java/com/IndianStateCensus/IndianStateCensusTest.java
@@ -8,8 +8,9 @@ import org.junit.jupiter.api.Test;
 
 class IndianStateCensusTest {
 	String IndiaStateCensus = "C:\\Users\\leena\\eclipse-workspace\\IndianStateCensus\\IndiaStateCensusData.csv";
-private static final String 	Incorrect_CSVFile = "C:\\Users\\leena\\eclipse-workspace\\Indian StateCensus\\IndiaStateCensusData.csv";
-	
+	String	Incorrect_CSVFile = "C:\\Users\\leena\\eclipse-workspace\\Indian StateCensus\\IndiaStateCensusData.csv";
+	String	Incorrect_CSVFile_Path = "C:\\Users\\leena\\eclipse-workspace\\IndiaStateCensusData.txt";
+
 	/**
 	 * UC1_TC 1.1_Returns number of records in datacsv file
 	 */
@@ -22,22 +23,38 @@ private static final String 	Incorrect_CSVFile = "C:\\Users\\leena\\eclipse-work
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * UC1_TC 1.2_Returns custom exception when incorrect path is input
 	 * @throws IOException
 	 */
 
 	@Test
-	void givenWrongPath_ReturnsCustomException() throws IOException {
+	void givenWrongTypeOfFile_ReturnsCustomException() throws IOException {
 		StateCensusAnalyser test = new StateCensusAnalyser();
 		int count = 0;
 		try {
 			count = test.loadStateCSVData(Incorrect_CSVFile);
 		} catch (CensusAnalyserExecption e) {
-			e.printStackTrace();
+			//e.printStackTrace();
 			assertEquals(CensusAnalyserExecption.ExceptionType.NO_FILE, e.type);
 		}
 	}
+	
+	/**
+	 * UC1_TC 1.3_Returns custom exception when incorrect extension given
+	 * @throws IOException
+	 */
+	@Test
+	void givenWrongPath_ReturnsCustomException() throws IOException {
+		StateCensusAnalyser test = new StateCensusAnalyser();
+		int count = 0;
+		try {
+			count = test.loadStateCSVData(Incorrect_CSVFile_Path);
+		} catch (CensusAnalyserExecption e) {
+			//e.printStackTrace();
+			assertEquals(CensusAnalyserExecption.ExceptionType.INCORRECT_FILE, e.type);
+		}
 	}
+}
 
